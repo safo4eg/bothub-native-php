85cbe182e7b5fe4bac9e099aa9f9b4184b0a7c8e
safo4eg <stas.rakov111@mail.ru>
2025-03-25 03:42:13 +0700

    feat: добавлена миграция таблицы пользователя

---
+++ migrations/Version20250324203400.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250324203400 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'create_users_table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('users');
+
+        $table->addColumn(name: 'id', typeName: 'bigint', options: [
+            'unsigned' => true,
+            'autoincrement' => true,
+        ]);
+
+        $table->setPrimaryKey(['id']);
+
+        $table->addColumn(name: 'balance', typeName: 'bigint', options: [
+            'unsigned' => true,
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('users');
+    }
+}
