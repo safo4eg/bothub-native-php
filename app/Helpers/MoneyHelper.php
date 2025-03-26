92108494d66c9cd4ee444877db02a0aabd22d0c3
safo4eg <stas.rakov111@mail.ru>
2025-03-26 17:24:33 +0700

    feat: добавлен мидлвар

---
+++ app/Helpers/MoneyHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers;
+
+class MoneyHelper
+{
+    public static function fromDollarsToCents(string $dollars): int
+    {
+        return (int) bcmul($dollars, '100', 0);
+    }
+
+    public static function fromCentsToDollars(string|int $cents): string
+    {
+        return bcdiv((string) $cents, '100', '0');
+    }
+}
--- app/Telegram/Middlewares/EnsureUserMiddleware.php
+++ app/Telegram/Middlewares/EnsureUserMiddleware.php
@@ -2,10 +2,29 @@
 
 namespace App\Telegram\Middlewares;
 
+/**
+ * Проверка на наличие записи пользователя в БД
+ * только для текстовых (по хорошему вынести в обработчик onCommand('start'))
+ */
+
 class EnsureUserMiddleware
 {
     public function __invoke($next, $update, $bot)
     {
+        $db = app()->get('db');
+        $chatId = $update->getMessage()->getChat()->getId();
+        $user = $db->fetchAssociative('SELECT * FROM users WHERE id = ?', [$chatId]);
+
+        $db->beginTransaction();
+        try {
+            if(!$user) {
+                $db->executeStatement('INSERT INTO users (id) VALUES (?)', [$chatId]);
+            }
+            $db->commit();
+        } catch (\Throwable $e) {
+            $db->rollBack();
+            throw $e;
+        }
 
         $next($update, $bot);
     }
